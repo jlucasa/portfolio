278af143f890db9abb47e9b30542262751ef7cf6
jlucasa <amen.jaredl@gmail.com>
2022-09-09 13:24:14 -0600

    Fix inconsistent important tagging from tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
